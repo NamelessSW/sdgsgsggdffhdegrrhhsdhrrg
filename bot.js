a1cbd1ce29b10bb74f0065b4a34731eb328c726c
xShaDowZx <36671650+xShaDowZx@users.noreply.github.com>
2018-07-07 01:06:24 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -694,10 +694,9 @@ message.channel.send(
 };
 });
 //embed
-var prefix = "#"
-
 client.on('message', message => {
   if (message.author.bot) return;
+	var prefix = "#"
   if (!message.content.startsWith(prefix)) return;
 
   let command = message.content.split(" ")[0];
