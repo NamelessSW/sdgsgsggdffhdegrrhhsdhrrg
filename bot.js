2a539d6bc5e8e91763a84dd99092a684d697aa96
xShaDowZx <36671650+xShaDowZx@users.noreply.github.com>
2018-07-06 03:22:52 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -305,5 +305,20 @@ message.channel.send({embed});
 
 });
 
+//member
+client.on('message', message => {
+    if(message.content == '#member') {
+    const embed = new Discord.RichEmbed()
+    .setDescription(`**Members info
+:green_heart: online:   ${message.guild.members.filter(m=>m.presence.status == 'online').size}
+:heart:dnd:       ${message.guild.members.filter(m=>m.presence.status == 'dnd').size}
+:yellow_heart: idle:      ${message.guild.members.filter(m=>m.presence.status == 'idle').size}   
+:black_heart: offline:   ${message.guild.members.filter(m=>m.presence.status == 'offline').size} 
+:blue_heart:   all:  ${message.guild.memberCount}**`)         
+         message.channel.send({embed});
+
+    }
+  });
+
 
 client.login(process.env.BOT_TOKEN);
