7d509544b5067799dc883bbb5ba9b3581858a6d3
xShaDowZx <36671650+xShaDowZx@users.noreply.github.com>
2018-07-07 23:54:58 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -592,67 +592,6 @@ client.on('guildMemberAdd', member => {
     let bots = member.guild.members.filter(m => m.user.bot).size
     member.guild.channels.get('453645288737996811').setName(`Total Bots: ${bots}`);
 });
-//server stats
-client.on("message",function(message) {
-  var prefix = "#"
-    if(message.content.startsWith(prefix + "st")) {
-           let uptime = client.uptime;
-
-    let days = 0;
-    let hours = 0;
-    let minutes = 0;
-    let seconds = 0;
-    let notCompleted = true;
-
-    while (notCompleted) {
-
-        if (uptime >= 8.64e+7) {
-
-            days++;
-            uptime -= 8.64e+7;
-
-        } else if (uptime >= 3.6e+6) {
-
-            hours++;
-            uptime -= 3.6e+6;
-
-        } else if (uptime >= 60000) {
-
-            minutes++;
-            uptime -= 60000;
-
-        } else if (uptime >= 1000) {
-            seconds++;
-            uptime -= 1000;
-
-        }
-
-        if (uptime < 1000)  notCompleted = false;
-
-    }
-let ms = 1000;
-let v1 = new Discord.RichEmbed()
-  v1.setTimestamp(new Date())
-  v1.setColor("RED")
-  v1.setDescription('***__ Collecting Data __***')
-  v1.setFooter("# | SenioR |") 
-let heroo = new Discord.RichEmbed()
-.setColor('RANDOM')
-.setTimestamp(new Date())
-.setThumbnail(client.user.avatarURL)
-.setAuthor(client.user.username,client.user.avatarURL)
-.addField("MyPrefix :",`**[ ${prefix} ]**`,true)
-.addField("Guilds :","**[ "+client.guilds.size+" ]**",true)
-.addField("Channels :","**[ "+client.channels.size+" ]**",true)
-.addField("Users :","**[ "+client.users.size+" ]**",true)
-.addField("MyName : ","**[ "+client.user.username+" ]**",true)
-.addField("MyID :","**[ "+client.user.id+" ]**",true)
-.addField("RamUsage :",`**[ ${(process.memoryUsage().rss / 1048576).toFixed()}MB ]**`,true)
-.addField("UpTime :",`**[** **Days:** \`${days}\` **Hours:** \`${hours}\` **Minutes:** \`${minutes}\` **Seconds:** \`${seconds}\` **]**`,true)
-.setFooter("SenioR TeaM. |")
-  message.channel.send({embed:v1}).then(m => m.edit({embed:heroo})),ms; 
-    }
-});
 //emoji 
 const mapping = {
   ' ': '   ',
@@ -761,7 +700,6 @@ let embed = new Discord.RichEmbed()
 .addField("**#bot :floppy_disk:**","**Info about the bot**")
 .addField("**#server :recycle:**","**For server information**")
 .addField("**#id :id: **", "**Shows your ID**")
-.addField("**#st :sparkle: **", "**Shows your Server stats**")
 .addField("**#member :hearts: **", "**Shows who everyone Status**")
 .addField("**#emoji :gem: **", "**Write your word in emoji**")
 .addField("**#flip :arrows_clockwise: **","**Flip your word**")
