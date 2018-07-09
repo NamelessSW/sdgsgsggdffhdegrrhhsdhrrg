2effc0edd0779d151c8f76cc21ebaed7f1ec036e
xShaDowZx <36671650+xShaDowZx@users.noreply.github.com>
2018-07-09 13:36:39 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1301,18 +1301,35 @@ client.on('message', message => {
     message.channel.sendEmbed(embed);
       }
   });
-//tag
-const figlet = require('figlet');
-client.on('message', message => {
+//achieve
+const sql = require("sqlite");
+client.on("message", async message => {
   var prefix = "#"
-if (message.content.startsWith(prefix + 'tag')) {
-    let args = message.content.split(" ").slice(1);
-if(!args[0]) return message.reply('Please write the text you want');  
-
-    figlet(args.join(" "), (err, data) => {
-              message.channel.send("```" + data + "```")
-           })
-}
+    if (message.content.startsWith(prefix + "achieve")) {
+         var ids = [
+            "20",
+            "1",
+            "13",
+            "18",
+            "17",
+            "9",
+            "31",
+            "22",
+            "23",
+            "2",
+            "11",
+            "19",
+            "24",
+            "25",
+            "12",
+            "33"
+            ]
+            const randomizer = Math.floor(Math.random()*ids.length);
+            const args = message.content.split(" ").slice(1).join(" ")
+    if (!args) return message.channel.send("Put something you want to achieve!");
+    const image = new Discord.Attachment(`https://www.minecraftskinstealer.com/achievement/a.php?i=${ids[randomizer]}&h=Achievement Get!&t=${args}`, "achievement.png");
+message.channel.send(image)
+    }
 });
 
 client.login(process.env.BOT_TOKEN);
